20751ee368dd9500404348f5a3570d95d9670ced
Marcos L. Souza <marcoslsouzadf@gmail.com>
2021-01-05 03:48:07 -0300

    Ajuste no metodo de salvar contato

--- src/main/java/com/github/marcoslsouza/agenda_api/api/rest/ContatoController.java
+++ src/main/java/com/github/marcoslsouza/agenda_api/api/rest/ContatoController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/contatos")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ContatoController {
 
     private final ContatoRepository repository;
