69fec4aeb2ca63c082974422d73cb091f0c59107
Marcos L. Souza <marcoslsouzadf@gmail.com>
2021-01-11 00:13:43 -0300

    Rotina para upload de fotos de contatos

--- src/main/java/com/github/marcoslsouza/agenda_api/api/rest/ContatoController.java
+++ src/main/java/com/github/marcoslsouza/agenda_api/api/rest/ContatoController.java
@@ -1,10 +1,15 @@
 package com.github.marcoslsouza.agenda_api.api.rest;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.cert.PKIXRevocationChecker.Option;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.Part;
 import javax.validation.Valid;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,8 +17,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,5 +62,26 @@ public class ContatoController {
     		c.setFavorito(c.getFavorito() == Boolean.TRUE ? false : true);
     		repository.save(c);
     	});
+    }
+    
+    // dominio/api/contatos/1/foto
+    @PutMapping("{id}/foto")
+    public byte[] addFoto(@PathVariable Long id, @RequestParam("foto") Part arquivo) {
+    	Optional<Contato> contato = repository.findById(id);
+    	// Converter contato para array de bytes
+    	return contato.map(c -> {
+    		try {
+    			// Representa a string do arquivo
+    			InputStream is = arquivo.getInputStream();
+    			byte[] bytes = new byte[(int) arquivo.getSize()];
+    			IOUtils.readFully(is, bytes);
+    			c.setFoto(bytes);
+    			repository.save(c);
+    			is.close();
+    			return bytes;
+    		} catch(IOException ex) {
+    			return null;
+    		}
+    	}).orElse(null);
     }
 }
--- src/main/java/com/github/marcoslsouza/agenda_api/model/entity/Contato.java
+++ src/main/java/com/github/marcoslsouza/agenda_api/model/entity/Contato.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.validation.constraints.NotEmpty;
 
 import lombok.Getter;
@@ -31,4 +32,8 @@ public class Contato {
 	
 	@Column
 	private Boolean favorito;
+	
+	@Column
+	@Lob
+	private byte[] foto;
 }
